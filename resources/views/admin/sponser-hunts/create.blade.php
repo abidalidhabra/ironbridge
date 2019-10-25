b3ee8300faf287b937c441ca983d9a1bd0bb15d2
Foremost Digital <info@foremostdigital.com>
2019-10-25 10:18:14 +0530

    spell added of add season

--- resources/views/admin/sponser-hunts/create.blade.php
+++ resources/views/admin/sponser-hunts/create.blade.php
@@ -10,7 +10,7 @@
             </div>
             <div class="col-md-12">
                 <div class="row">
-                    <h3>Add Session</h3>
+                    <h3>Add Season</h3>
                 </div>
             </div>
         </div>
