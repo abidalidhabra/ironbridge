c1d7198a47fc6f9742ea5bd552705e9eacb371d7
Foremost Digital <info@foremostdigital.com>
2020-01-22 15:33:30 +0530

    1. changing the avatar item xp reward format. 2. account was reset. 3. Show Server details in admin panel.

---
+++ config/servers.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'url' => [
+    	
+    	'production' => env('PRODUCTION_URL', ''),
+    	
+    	'staging' => env('STAGING_URL', ''),
+    	
+    	'local' => env('LOCAL_URL', '')
+    ]
+];
--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -1,11 +1,18 @@
+@php
+    $env = config('app.env');
+    $envPath = config('servers.url.'.$env);
+    $oppEnv = ($env == 'staging')? 'production': 'staging';
+    $oppEnvPath = config('servers.url.'.$oppEnv);
+@endphp
 <header>
     <div class="hamburgerboxdis">
         <a id="offCanvas" class="hamburger"><span> ☰ </span></a>
     </div>
     <div class="logo_leftbox">
         <img src="{{ asset('admin_assets/svg/ib-logo.svg') }}">
-        <div class="headerLabel">
-            {{ strtoupper(config('app.env')) . ' SERVER' }}
+        <div class="headerLabel left_label Label_active">
+            <h5>{{ strtoupper($env) }} SERVER</h5>
+            <p>{{ $envPath }}</p>
         </div>
     </div>
     <div class="logout_rightbox">
@@ -13,6 +20,11 @@
                             document.getElementById('logout-form').submit();">
             <h4>LOG OUT</h4>
         </a>
+        <div class="headerLabel right_label">
+            <h5>{{ strtoupper($oppEnv) }} SERVER</h5>
+            <p>{{ $oppEnvPath }}</p>
+        </div>
+        
         <form id="logout-form" action="{{ route('admin.logout') }}" method="POST" style="display: none;">
             @csrf
         </form>
