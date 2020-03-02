a65cb6394077c6ae81d5c3ebab082495d0836248
abidali <abid@zillionweb.com>
2020-03-02 19:39:08 +0530

    userlist in orde by remove

--- resources/views/admin/user/userList.blade.php
+++ resources/views/admin/user/userList.blade.php
@@ -95,7 +95,6 @@
                 processing: true,
                 responsive: true,
                 serverSide: true,
-                // order:[],
                 lengthMenu: [[10, 50, 100, -1], [10, 50, 100, "All"]],
                 ajax: {
                     type: "get",
