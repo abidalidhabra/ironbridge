7a2f91f425d77b993a0f79825cee57a3d5439503
Foremost Digital <info@foremostdigital.com>
2020-03-07 13:36:15 +0530

    ranking leaderboard api has been updated

--- app/Services/Event/LeaderBoardService.php
+++ app/Services/Event/LeaderBoardService.php
@@ -28,6 +28,8 @@ class LeaderBoardService
     public $userIds;
     public $users;
     public $myRank;
+    public $beforeCursor;
+    public $afterCursor;
 
 	public function __construct()
 	{
@@ -56,35 +58,23 @@ class LeaderBoardService
 		return $this->response();
 	}
 
-  public function next($direction,int $cursor)
+  public function next($direction, int $cursor)
   {
-    $this->response = $this->more($direction, $cursor);
+    $this->more($direction, $cursor);
+    $this->getUsers();
+    $this->map($cursor);
     return $this->response();
   }
 
     public function toppers()
     {
-        $this->toppers = EventUser::where('event_id', $this->event->id)
-        ->select('_id', 'user_id', 'event_id', 'compasses')
-        ->orderBy('compasses.remaining', 'desc')
-        ->limit(3)
-        ->get();
-        $this->userIds = array_merge($this->userIds, $this->toppers->pluck('user_id')->toArray(), [auth()->user()->id]);
-        return $this->toppers;
-    }
-
-    public function me()
-    {
-        $userRank = EventUser::raw(function($collection) {
+        $this->toppers = EventUser::raw(function($collection) {
                 return $collection->aggregate([
                     [
-                        '$match'=> [
-                            'user_id'=> auth()->user()->id,
-                            'event_id'=> $this->event->id,
-                        ]
+                        '$sort'=> ['compasses.remaining'=> -1]
                     ],
                     [
-                        '$sort'=> ['compasses.remaining'=> -1]
+                        '$limit'=> 3
                     ],
                     [
                         '$project'=> [
@@ -111,22 +101,67 @@ class LeaderBoardService
                 ]);
             });
 
+        $this->toppers->map(function($user){
+            $user->rank += 1;
+            return $user;
+        });
+
+        $this->userIds = array_merge($this->userIds, $this->toppers->pluck('participations')->pluck('user_id')->toArray(), [auth()->user()->id]);
+
+        return $this->toppers;
+    }
+
+    public function me()
+    {
+        $userRank = EventUser::raw(function($collection) {
+                return $collection->aggregate([
+                    [
+                        '$sort'=> ['compasses.remaining'=> -1]
+                    ],
+                    [
+                        '$project'=> [
+                            '_id'=> true,
+                            'user_id'=> true,
+                            'event_id'=> true,
+                            'compasses'=> true,
+                        ]
+                    ],
+                    [
+                        '$group' => [
+                            '_id'   => null,
+                            'participations' => [
+                                '$push'  => '$$ROOT'
+                            ]
+                        ]
+                    ],
+                    [
+                        '$unwind' => [
+                            'path'=> '$participations',
+                            'includeArrayIndex'=> 'rank',
+                        ]
+                    ],
+                    [
+                        '$match'=> [
+                            'participations.user_id'=> auth()->user()->id,
+                            'participations.event_id'=> $this->event->id,
+                        ]
+                    ]
+                ]);
+            });
+
         if ($me = $userRank->first()) {
             $me->rank += 1;
-            if ($me->rank > 3) {
-                unset($me->items->widgets);
-            }
+            // if ($me->rank > 3) {
+            //     unset($me->items->widgets);
+            // }
             $this->myRank = $me->rank;
             return $this->me = collect([$me]);
         }
     }
 
-	public function more($direction, $cursor)
-	{
+    public function more($direction, $cursor)
+    {
 
-        // for ($i=0; $i < 150; $i++) { 
-        //     $digits[] = $i+1;
-        // }
         $paginate = 3;
         if ($direction == 'up') {
             $limit = (($cursor - 1) == 0)? 0: ($cursor - 1);
@@ -141,27 +176,65 @@ class LeaderBoardService
             $limit = $paginate;
         }
 
-		// dd($skip, $limit, $cursor, $paginate);
-        // $data = collect($digits)->slice($skip)->take($limit)->values();
-		$data = EventUser::where('event_id', $this->event->id)
-        		->select('_id', 'event_id', 'user_id', 'compasses')
-        		->orderBy('compasses.remaining', 'desc')
-        		->skip($skip)
-        		->limit($limit)
-        		->get();
-        $this->userIds = array_merge($this->userIds, $data->pluck('user_id')->toArray());
+        if ($limit) {
+            $data = EventUser::raw(function($collection) use ($skip, $limit) {
+                    return $collection->aggregate([
+                        [
+                            '$sort'=> ['compasses.remaining'=> -1]
+                        ],
+                        [
+                            '$skip'=> $skip
+                        ],
+                        [
+                            '$limit'=> $limit
+                        ],
+                        [
+                            '$project'=> [
+                                '_id'=> true,
+                                'user_id'=> true,
+                                'event_id'=> true,
+                                'compasses'=> true,
+                            ]
+                        ],
+                        [
+                            '$group' => [
+                                '_id'   => null,
+                                'participations' => [
+                                    '$push'  => '$$ROOT'
+                                ]
+                            ]
+                        ],
+                        [
+                            '$unwind' => [
+                                'path'=> '$participations',
+                                'includeArrayIndex'=> 'rank',
+                            ]
+                        ]
+                    ]);
+                });
+            $data->map(function($user){
+                $user->rank += 1;
+            });
+        }else{
+            $data = collect();
+        }
 
-		if ($direction == 'down' && ($data->count() == 0 || $data->count() < $paginate)) {
-			$nextCursor = 0;
-		}
+
+        $this->userIds = array_merge($this->userIds, $data->pluck('participations')->pluck('user_id')->toArray());
+
+        if ($direction == 'down' && ($data->count() == 0 || $data->count() < $paginate)) {
+            $nextCursor = 0;
+        }
 
         if ($direction == 'up') {
+            $this->beforeCursor = ($nextCursor)? $nextCursor: 0;
             $this->before = $data;
         }else{
+            $this->afterCursor = ($nextCursor)? $nextCursor: 0;
             $this->after = $data;
         }
-		return [$direction.'_data'=> $data, 'cursor'=> ($nextCursor)? $nextCursor+1: 0];
-	}
+        return [$direction.'_data'=> $data, 'cursor'=> ($nextCursor)? $nextCursor+1: 0];
+    }
 
     public function getUsers()
     {
@@ -176,42 +249,71 @@ class LeaderBoardService
         return $this;
     }
 
-    public function map()
+    public function map($cursor = null)
     {
         $users = collect($this->users);
-        $this->response['toppers'] = $this->toppers->map(function($user) use ($users){
-            $temp = $users->where('_id', $user->user_id)->values()->first();
-            $user->first_name = $temp['first_name'];
-            $user->last_name = $temp['last_name'];
-            $user->avatar = $temp['avatar'];
-            $user->widgets = $temp['widgets'];
-            $user->gender = $temp['gender'];
-            return $user;
-        });
+        
 
-        $this->response['me'] = $this->me->map(function($user) use ($users){
-            $temp = $users->where('_id', $user->participations->user_id)->values()->first();
-            $user->first_name = $temp['first_name'];
-            $user->last_name = $temp['last_name'];
-            $user->avatar = $temp['avatar'];
-            return $user;
-        });
-
-        $this->response['before'] = $this->before->map(function($user) use ($users){
-            $temp = $users->where('_id', $user->user_id)->values()->first();
-            $user->first_name = $temp['first_name'];
-            $user->last_name = $temp['last_name'];
-            $user->avatar = $temp['avatar'];
-            return $user;
-        });
-
-        $this->response['after'] = $this->after->map(function($user) use ($users){
-            $temp = $users->where('_id', $user->user_id)->values()->first();
-            $user->first_name = $temp['first_name'];
-            $user->last_name = $temp['last_name'];
-            $user->avatar = $temp['avatar'];
-            return $user;
-        });
+        if($this->toppers){
+            $this->response['toppers'] = $this->toppers->map(function($user) use ($users){
+                $temp = $users->where('_id', $user->participations->user_id)->values()->first();
+                $user->user_id = $temp['_id'];
+                $user->first_name = $temp['first_name'];
+                $user->last_name = $temp['last_name'];
+                $user->avatar = $temp['avatar'];
+                $user->widgets = collect($temp['widgets'])->where('selected', true);
+                $user->gender = $temp['gender'];
+                $user->compasses = $user->participations->compasses;
+                unset($user->participations, $user->_id);
+                return $user;
+            });
+        }
+        
+        if($this->me){
+            $this->response['me'] = $this->me->map(function($user) use ($users){
+                $temp = $users->where('_id', $user->participations->user_id)->values()->first();
+                $user->user_id = $temp['_id'];
+                $user->first_name = $temp['first_name'];
+                $user->last_name = $temp['last_name'];
+                $user->avatar = $temp['avatar'];
+                $user->compasses = $user->participations->compasses;
+                if ($user->rank <= 3) {
+                    $user->widgets = collect($temp['widgets'])->where('selected', true);
+                }
+                unset($user->participations, $user->_id);
+                return $user;
+            });
+        }
+        
+        if($this->before){
+            $this->response['before'] = $this->before->map(function($user) use ($users, $cursor){
+                $temp = $users->where('_id', $user->participations->user_id)->values()->first();
+                $user->user_id = $temp['_id'];
+                $user->first_name = $temp['first_name'];
+                $user->last_name = $temp['last_name'];
+                $user->avatar = $temp['avatar'];
+                $user->rank = ($cursor)? $cursor + $user->rank: $this->myRank - $user->rank;
+                $user->compasses = $user->participations->compasses;
+                unset($user->participations, $user->_id);
+                return $user;
+            });
+            $this->response['before_cursor'] = $this->beforeCursor;
+        }
+        
+        if($this->after){
+            $this->response['after'] = $this->after->map(function($user) use ($users, $cursor){
+                $temp = $users->where('_id', $user->participations->user_id)->values()->first();
+                $user->user_id = $temp['_id'];
+                $user->first_name = $temp['first_name'];
+                $user->last_name = $temp['last_name'];
+                $user->avatar = $temp['avatar'];
+                $user->compasses = $user->participations->compasses;
+                $user->rank = ($cursor)? $cursor + $user->rank: $this->myRank + $user->rank;
+                unset($user->participations, $user->_id);
+                return $user;
+            });
+            $this->response['after_cursor'] = $this->afterCursor;
+        }
     }
 
 	public function response()
