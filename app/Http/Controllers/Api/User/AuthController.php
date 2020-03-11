f5c0bc92ae40d5ed619184e7d8b10e4b3836da4f
kishan <info@foremostdigital.com>
2020-03-11 19:30:49 +0530

    auth

--- app/Http/Controllers/Api/User/AuthController.php
+++ app/Http/Controllers/Api/User/AuthController.php
@@ -93,7 +93,7 @@ class AuthController extends Controller
         if ($validator->fails()) {
             return response()->json(['message'=>$validator->messages()], 422);
         }
-         $user =User::where('_id', $request->guestid)->get();
+         $user =User::find($request->guestid);
          if ($request->longitude || $request->latitude) {
                     $wantToSave = true;
                     $user->location = [
