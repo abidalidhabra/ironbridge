35d09d5a4bf3c291858172aa437223186ffd12de
Prashant Rajavat <prashant.rajawat1996@gmail.com>
2020-04-01 19:38:55 +0530

    changed docker configuration.

--- .docker/startup.sh
+++ .docker/startup.sh
@@ -2,6 +2,10 @@
 
 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf &
 
-echo "cron starting..." && (cron) && : > /var/log/cron.log &
+# echo "cron starting..." && (cron) && : > /var/log/cron.log &
+
+crontab /etc/cron.d/cron
+
+cron -f
 
 apache2-foreground
